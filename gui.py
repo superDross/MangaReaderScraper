858c9274300428146d23b615d253f136c9a2d7bb
David Ross <david.ross@yougov.com>
2019-08-04 18:01:51 +0100

    Fixed requirements and added docstrings to gui classes

--- gui.py
+++ gui.py
@@ -26,12 +26,14 @@ def download_manga(manga, volume):
         downloader.download_all_volumes()
 
 
-class ResultsTable(qtw.QTableWidget):
+class ResultsTable(qtw.QTabsWidget):
+    """ Search results in a tabeled format. """
+
     def __init__(self):
         super().__init__()
         self.results = None
 
-    def _set_headers(self):
+    def _configure_headers(self):
         self.setColumnCount(4)
         self.setHorizontalHeaderLabels(["Title", "Chapter", "Type", ""])
         self.horizontalHeader().setSectionResizeMode(0, qtw.QHeaderView.Stretch)
@@ -39,6 +41,7 @@ class ResultsTable(qtw.QTableWidget):
 
     @staticmethod
     def create_combo_widget(num):
+        """ Creates a combobox with selection from 0 to parsed num."""
         combobox = qtw.QComboBox()
         combobox.addItem("all")
         for i in range(1, int(num) + 1):
@@ -46,15 +49,17 @@ class ResultsTable(qtw.QTableWidget):
         return combobox
 
     def _add_row(self, row_num, name, url, chapters, _type):
-        self.setItem(row_num, 0, qtw.QTableWidgetItem(name))
+        """ Search result row with chapter selection and download button."""
+        self.setItem(row_num, 0, qtw.QTabsWidgetItem(name))
         combo = self.create_combo_widget(chapters)
         self.setCellWidget(row_num, 1, combo)
-        self.setItem(row_num, 2, qtw.QTableWidgetItem(_type))
+        self.setItem(row_num, 2, qtw.QTabsWidgetItem(_type))
         download_button = qtw.QPushButton("Download")
         download_button.clicked.connect(partial(self.on_click, url, combo))
         self.setCellWidget(row_num, 3, download_button)
 
     def _add_rows(self):
+        """ Transforms the search results into rows."""
         row_num = 0
         for result in self.results.items():
             name, url, chapters, _type = list(result[1].values())
@@ -62,19 +67,23 @@ class ResultsTable(qtw.QTableWidget):
             row_num += 1
 
     def construct(self, results):
+        """ Creates the tabel."""
         self.results = results
         self.setRowCount(len(results.keys()))
-        self._set_headers()
+        self._configure_headers()
         self._add_rows()
 
     @qtc.pyqtSlot()
     def on_click(self, url, combo):
+        """ Downloads the selected manga."""
         volume = combo.currentText() if combo.currentText() != "any" else None
         print(f"selected {url} volume {volume}")
         download_manga(url, volume)
 
 
 class SearchTab(qtw.QWidget):
+    """ Tab allowing user to search and select mangas."""
+
     def __init__(self):
         super().__init__()
         self.name = "Search"
@@ -88,20 +97,24 @@ class SearchTab(qtw.QWidget):
         self._configure()
 
     def _text_label(self):
+        """ Configures the text label."""
         self.layout.addWidget(self.text_label, 0, 0)
 
     def _search_line(self):
+        """ Configures search box."""
         self.search_line.setMaximumSize(200, 25)
         self.search_line.resize(20, 20)
         self.search_line.move(15, 1)
         self.layout.addWidget(self.search_line, 1, 0)
 
     def _submit_button(self):
+        """ Configures search submission button."""
         self.submit_button.clicked.connect(self.on_click)
         self.submit_button.setMaximumSize(80, 25)
         self.layout.addWidget(self.submit_button, 1, 1)
 
     def _table_box(self):
+        """ Configures the search result table."""
         self.layout.addWidget(self.table, 2, 0)
 
     def _configure(self):
@@ -113,7 +126,7 @@ class SearchTab(qtw.QWidget):
 
     @qtc.pyqtSlot()
     def on_click(self):
-        print(self.search_line.text())
+        """ Used to submit query for search."""
         self.user_input = self.search_line.text()
         search = Search()
         search.search(self.user_input)
@@ -122,12 +135,16 @@ class SearchTab(qtw.QWidget):
 
 
 class SelectionTab(qtw.QWidget):
+    """ Tab allowing the user to download a known manga & volume."""
+
     def __init__(self):
         super().__init__()
         self.name = "Selection"
 
 
-class TableWidget(qtw.QWidget):
+class TabsWidget(qtw.QWidget):
+    """ All tabs in the main window."""
+
     def __init__(self, parent, height, width):
         super().__init__(parent)
         self.layout = qtw.QVBoxLayout(self)
@@ -145,6 +162,8 @@ class TableWidget(qtw.QWidget):
 
 
 class App(qtw.QMainWindow):
+    """ Primary GUI window."""
+
     def __init__(self):
         super().__init__()
         self.title = "MangaReaderScraper"
@@ -159,7 +178,7 @@ class App(qtw.QMainWindow):
         # set initial position on desktop
         self.setGeometry(self.left, self.top, self.width, self.height)
         self.statusBar().showMessage("In progress")
-        self.setCentralWidget(TableWidget(self, self.height, self.width))
+        self.setCentralWidget(TabsWidget(self, self.height, self.width))
         self.show()
 
 
