9ac2f50b8e691354391f4684ec8e5a1c37a49602
David <dross78375@gmail.com>
2019-12-08 16:33:46 +0000

    Remove config python file

--- scraper/config.py
+++
@@ -1,8 +0,0 @@
-""" Stores global variables used across multiple modules."""
-import os
-
-# TODO: place this in a utils function
-HERE = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-
-# TODO: put all the below vars in a settings.cfg file
-MANGA_DIR = os.path.join(HERE, "mangas")
